cb43b66d2b0735fe212f82f4ec5097b34941ebe8
alan <alanprotesto@gmail.com>
2025-09-23 23:40:31 -0300

    finalização do projeto front

---
+++ README.md
@@ -0,0 +1,61 @@
+# 📌 Front Crédito – Documentação
+
+Projeto **front-end** para consumir a API de crédito.
+
+🎥 **Demonstração no YouTube:**  
+[▶️ Assista aqui](https://youtu.be/u0AqadWLnXY)
+
+---
+
+## 🚀 Tecnologias Utilizadas
+
+- **Node.js:** `v22.14.0`
+- **Docker**
+- **Angular:** `@angular/cli 19.2.6`
+- **Bootstrap:** `5.x`
+
+---
+
+## 🛠️ Como Rodar o Projeto
+
+### 1️⃣ Clonar o Repositório
+
+No terminal, execute:
+
+```bash
+git clone https://github.com/alan-santos-helloworld2020/front-credito.git
+```
+
+---
+
+### 2️⃣ Gerar o Build do Projeto
+
+Acesse a pasta raiz do projeto e execute:
+
+```bash
+docker build -t angular-docker .
+```
+
+> ⏳ **Aguarde a conclusão do processo de build.**
+
+---
+
+### 3️⃣ Subir o Container
+
+Execute o comando:
+
+```bash
+docker run -p 4201:4200 angular-docker
+```
+
+---
+
+### 4️⃣ Acessar no Navegador
+
+Abra o navegador e acesse:
+
+🔗 http://localhost:4201
+
+---
+
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DEFAULT_CURRENCY_CODE, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CreditoService } from './services/credito.service';
-import { Credito } from './interfaces/credito';
-import { CurrencyPipe, DatePipe, PercentPipe } from '@angular/common';
+import { CurrencyPipe, DatePipe, PercentPipe, JsonPipe, registerLocaleData } from '@angular/common';
 import { CreditoDto } from './interfaces/credito-dto';
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CurrencyPipe, DatePipe, PercentPipe],
+  imports: [
+    RouterOutlet,
+    CurrencyPipe,
+    DatePipe,
+    PercentPipe
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR'
+    },
+    {
+      provide:  DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL'
+    },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 
@@ -20,21 +37,30 @@ export class AppComponent implements OnInit {
 
   constructor(private service: CreditoService) { }
 
+
   ngOnInit(): void {
+    this.buscarDados();
+  }
+
+  buscarDados() {
     this.service.getCreditos().subscribe(creditos => {
       this.creditos = creditos;
-
     });
   }
 
   onChange(event: any) {
     let numeroNfse: string = event.target!['value'];
-    this.service.getCreditoByNumeroNsfe(numeroNfse).subscribe(credito => {
 
+    try {
+      this.service.getCreditoByNumeroNsfe(numeroNfse).subscribe(credito => {
         this.creditos = [credito];
+      });
+    } catch (e) {
+      console.log(e);
+      this.buscarDados();
+    }
 
-      
-    });
+
   }
 
 
